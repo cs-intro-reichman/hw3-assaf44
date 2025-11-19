6263595037c09695213a426e646751495de4ea3c
ASSAF44 <assaf.barel@post.runi.ac.il>
2025-11-19 14:03:47 +0200

    completed assignment

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,126 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int counter = x2;
+			while (counter > 0) {
+				x1++;
+        		counter--;
+    		} while (counter < 0) {
+				x1--;
+				counter++;
+			}
+	return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int counter = x2;
+			while (counter < 0) {
+				x1++;
+        		counter++;
+    		} while (counter > 0) {
+				x1--;
+				counter--;
+			}
+	return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+			if (x1==0 || x2==0){
+				return 0;
+			}
+		int absx1 = x1;
+			if (x1 < 0) {
+				absx1 = minus(0, x1);
+			}
+		int absx2 = x2;
+			if (x2 < 0) {
+				absx2 = minus(0, x2);
+			}
+		int result = 0;
+			for(int i = 0; i < absx2; i = plus(i,1)) {
+				result = plus(result, absx1);
+			}
+			if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)){
+				result = minus (0, result);
+			}
+	return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+			if (x == 0){
+				return 0;
+			}
+			if (n == 0){
+				return 1;
+			}
+		int result = x;
+			for (int i = 0; i < minus(n,1); i = plus(i,1)){
+				result = (times(result,x));
+			}
+	return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+			if (x2 == 0 || x1<x2){
+				return 0;
+			}
+			int absx1 = x1;
+			if (x1 < 0) {
+				absx1 = minus(0, x1);
+			}
+		int absx2 = x2;
+			if (x2 < 0) {
+				absx2 = minus(0, x2);
+			}
+		int result = 0;
+		int remainder = absx1;
+			while (remainder >= absx2) {
+				remainder = minus(remainder,absx2);
+				result++;
+			}
+			if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)){
+				result = minus (0, result);
+			}
+	return result;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+			if (x2 == 0){
+				return x1;
+			}
+			if (x1 < x2){
+				return x1;
+			}
+		int quotient = div(x1,x2);
+		int product = times(quotient, x2);
+		int remainder = minus(x1,product);
+	return remainder;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+			if (x <= 0){
+				return 0;
+			}
+		int low = 1;
+		int high = x;
+		int result = 1;
+			while (low <= high){
+				int mid = div(plus(low, high), 2);
+				int midSquared = pow(mid,2);
+				if (midSquared == x) {
+            return mid;
+				}
+				if (midSquared < x){
+					result = mid;
+					low = plus(mid, 1);
+				} else {
+					high = minus(mid, 1);
+			}
+		return result;
 	}	  	  
-}
